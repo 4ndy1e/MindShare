0a48f787577797850eecba145c017cc8a3528b62
Andy Le <4ndyle@gmail.com>
2025-01-04 14:45:12 -0700

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+# __name__ is set to the name of the module that it is used (app) 
+# flask uses module passed here as a starting point when it needs to laod associated resources
+app = Flask(__name__)
+
+# place at bottom since routes module needs to import the app variable defined in this script
+from app import routes
