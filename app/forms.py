e8742473ac8a00128f10396ad91ab485ccc76ac5
Andy Le <4ndyle@gmail.com>
2025-01-05 18:39:19 -0700

    add comments for clarity

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
+  # these fields are ready to be placed into HTML, so we dont have to build HTML elements like usual
   username = StringField('Username', validators=[DataRequired()])
   password = PasswordField('Password', validators=[DataRequired()])
   remember_me = BooleanField('Rememebr me')
