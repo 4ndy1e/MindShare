16c7ae2a1ac3b854975f8d7049a80c1b26b07246
Andy Le <4ndyle@gmail.com>
2025-01-07 14:41:52 -0700

    feature: login functionality and password hashing

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # __name__ is set to the name of the module that it is used (app) 
 # flask uses module passed here as a starting point when it needs to laod associated resources
@@ -11,6 +12,8 @@ app.config.from_object(Config)
 # database represented from database instance
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+login = LoginManager(app)
 
 # place at bottom since routes module needs to import the app variable defined in this script
 from app import routes, models
