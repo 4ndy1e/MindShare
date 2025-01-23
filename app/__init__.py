57fc5282e22f6a7c8ec0e93f37de8f6bd730484b
Andy Le <4ndyle@gmail.com>
2025-01-22 18:32:12 -0700

    feature: error logging into files

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 # __name__ is set to the name of the module that it is used (app) 
 # flask uses module passed here as a starting point when it needs to laod associated resources
@@ -38,6 +39,17 @@ if not app.debug:
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
+    # enable file based logging
+    if not os.path.exists('logs'):
+      os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/mindshare.blog', maxBytes=10240, backupCount=10)
+
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('MindShare startup')
 
 # place at bottom since routes module needs to import the app variable defined in this script
 from app import routes, models, errors
